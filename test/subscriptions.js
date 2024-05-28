8aa8632523276768236c4d7845678fc561522b3f
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-05-28 10:57:54 -0500

    Update subscriptions.js

--- test/subscriptions.js
+++ test/subscriptions.js
@@ -1,12 +1,12 @@
 // subscription-dates.js
 const userNames = {
-    "4567": "Christian",
     "4061": "Preston",
+    "4567": "Christian",
     // Add more user names as needed
 };
 
 const subscriptionDates = {
+    "4061": "2024-07-01",
     "4567": "2024-08-15",
-    "4061"; "2025-05-28",
     // Add more codes and dates as needed
 };
